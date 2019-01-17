a9b47040744de36ab943b8086c2642cb34e148b5
O'brien <OBRIENJOH@ENT.dfo-mpo.ca>
2019-01-17 13:42:25 -0400

    Insert shortcuts to major code chunks

--- Code/GriddingCode_GRIDS_JOB.R
+++ Code/GriddingCode_GRIDS_JOB.R
@@ -7,7 +7,7 @@ library(rgdal)
 library(raster)
 library(sp)
 
-#Maritimes
+####Maritimes####
 
 MaritimesSA <- readOGR("Data/Shapefiles/MaritimesStudyArea.shp")
 LandMaritimes <- readOGR("Data/Shapefiles/LandBordersMaritimes.shp")
@@ -35,10 +35,10 @@ rm(list=setdiff(ls(), "Griddata"))
 
 save.image("Data/MaritimeGridsWholeCell.RData")
 
-#Newfoundland
+####Newfoundland####
 
-NL_SA <- readOGR('Data/Shapefiles/NL_RVsurveyAgg.shp')
-LandMaritimes <- readOGR("Data/Shapefiles/LandBordersMaritimes.shp")
+study_area <- readOGR('Data/Shapefiles/NL_RVsurveyAgg.shp')
+Land <- readOGR("Data/Shapefiles/NL_landborders.shp")
 
 grids <- c(75000,70000,65000,60000,55000,50000,
            45000,40000,35000,30000,25000,20000,
@@ -51,14 +51,13 @@ for (i in 1:length(grids)){
   
   print(paste("working on grid ~ ",grids[i]/1000,"km resolution",sep=" "))
   
-  tempgrid <- GridFilter2(MaritimesSA, resol=grids[i], LandMaritimes)
+  tempgrid <- GridFilter2(study_area, resol=grids[i], Land)
   
   Griddata[[i]] <- tempgrid
-  plot(tempgrid,main=paste(grids[i]/1000,"km resolution",sep=" "))
   
   rm(tempgrid)
 }
 
 rm(list=setdiff(ls(), "Griddata"))
 
-save.image("Data/MaritimeGridsWholeCell.RData")
+save.image("Data/NLGridsWholeCell.RData")
