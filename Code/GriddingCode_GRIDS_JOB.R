2ba18884fb40eaf7fbef90e814410f18d3b04bf8
O'brien <OBRIENJOH@ENT.dfo-mpo.ca>
2019-04-01 09:13:47 -0300

    add code sections for sGOSL and nGOSL grids

--- Code/GriddingCode_GRIDS_JOB.R
+++ Code/GriddingCode_GRIDS_JOB.R
@@ -61,3 +61,58 @@ for (i in 1:length(grids)){
 rm(list=setdiff(ls(), "Griddata"))
 
 save.image("Data/NLGridsWholeCell.RData")
+
+####Gulf####
+
+study_area <- readOGR('Data/Shapefiles/Gulf_RVsurveyAgg.shp')
+Land <- readOGR("Data/Shapefiles/Gulf_landborders.shp")
+
+grids <- c(75000,70000,65000,60000,55000,50000,
+           45000,40000,35000,30000,25000,20000,
+           15000,10000,9000,8000,7000,6000,5000,
+           4000,3000)
+
+Griddata <- list()
+
+for (i in 1:length(grids)){
+  
+  print(paste("working on grid ~ ",grids[i]/1000,"km resolution",sep=" "))
+  
+  tempgrid <- GridFilter2(study_area, resol=grids[i], Land)
+  
+  Griddata[[i]] <- tempgrid
+  
+  rm(tempgrid)
+}
+
+rm(list=setdiff(ls(), "Griddata"))
+
+save.image("Data/GulfGridsWholeCell.RData")
+
+####Quebec####
+
+study_area <- readOGR('Data/Shapefiles/QC_StudyArea.shp')
+Land <- readOGR("Data/Shapefiles/QC_landborders.shp")
+
+grids <- c(75000,70000,65000,60000,55000,50000,
+           45000,40000,35000,30000,25000,20000,
+           15000,10000,9000,8000,7000,6000,5000,
+           4000,3000)
+
+Griddata <- list()
+
+for (i in 1:length(grids)){
+  
+  print(paste("working on grid ~ ",grids[i]/1000,"km resolution",sep=" "))
+  
+  tempgrid <- GridFilter2(study_area, resol=grids[i], Land)
+  
+  Griddata[[i]] <- tempgrid
+  
+  rm(tempgrid)
+}
+
+rm(list=setdiff(ls(), "Griddata"))
+
+save.image("Data/QCGridsWholeCell.RData")
+
