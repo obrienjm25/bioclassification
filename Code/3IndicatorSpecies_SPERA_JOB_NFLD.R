2c5a6b9efb89dd3dee2a4ff01d04d7f3da1f8037
O'brien <OBRIENJOH@ENT.dfo-mpo.ca>
2019-04-01 09:07:21 -0300

    Updated indicator analysis with fall and spring surveys combined

--- Code/3IndicatorSpecies_SPERA_JOB_NFLD.R
+++ Code/3IndicatorSpecies_SPERA_JOB_NFLD.R
@@ -25,21 +25,21 @@ colorGridData<-colorGridData[,-1] #remove superfluous index variable
 top5 <- names(sort(table(colorGridData$cl), decreasing = T))[1:5] #list of top 5 clusters
 
 colorGridData_top5<-colorGridData[colorGridData$cl %in% top5,] #create subset df of top 5 clusters
-PA_data<-colorGridData_top5[,c(-1,-71,-72)] #remove columns to create strictly sitexspecies PA matrix
+PA_data<-colorGridData_top5[,c(-1,-73,-74)] #remove columns to create strictly sitexspecies PA matrix
 
 colorGridData_top5$cl2[colorGridData_top5$cl==9]<-1 #assign top 5 clusters new numeric cluster membership
-colorGridData_top5$cl2[colorGridData_top5$cl==8]<-2 #between 1 and 5 in order of largest to smallest
-colorGridData_top5$cl2[colorGridData_top5$cl==12]<-3
+colorGridData_top5$cl2[colorGridData_top5$cl==11]<-2 #between 1 and 5 in order of largest to smallest
+colorGridData_top5$cl2[colorGridData_top5$cl==8]<-3
 colorGridData_top5$cl2[colorGridData_top5$cl==1]<-4
-colorGridData_top5$cl2[colorGridData_top5$cl==5]<-5
+colorGridData_top5$cl2[colorGridData_top5$cl==4]<-5
 
 
 #Provide a name to each cluster associated with broad geomorphic features
-colorGridData_top5$ecoregion[colorGridData_top5$cl==9]<- "Outer Shelf" 
-colorGridData_top5$ecoregion[colorGridData_top5$cl==8]<- "Inner Shelf"
-colorGridData_top5$ecoregion[colorGridData_top5$cl==12]<- "Grand Banks"
+colorGridData_top5$ecoregion[colorGridData_top5$cl==9]<- "Inner Shelf" 
+colorGridData_top5$ecoregion[colorGridData_top5$cl==11]<- "Outer Shelf"
+colorGridData_top5$ecoregion[colorGridData_top5$cl==8]<- "Grand Banks"
 colorGridData_top5$ecoregion[colorGridData_top5$cl==1]<- "Slope"
-colorGridData_top5$ecoregion[colorGridData_top5$cl==5]<- "Grand Banks: SW Margin"
+colorGridData_top5$ecoregion[colorGridData_top5$cl==4]<- "Laurentian Channel/Shelf Break"
 
 
 colorGridData_top5$cl2<-as.factor(colorGridData_top5$cl2)
@@ -62,15 +62,15 @@ indmax <- indicators2$indcls #get indVal for species in its maximum class
 maxfreq <- pmax(relfreq[1], relfreq[2],relfreq[3],relfreq[4],relfreq[5]) # gives relative frequency for species in its max class
 names(maxfreq) <- "maxfreq"
 
-names1<-c(paste("freq_", c("1_OS","2_IS",
+names1<-c(paste("freq_", c("1_IS","2_OS",
                            "3_GB", "4_Slope", 
-                           "5_SW_margin_GB"), sep=""))
-names2<-c(paste("abund_", c("1_OS","2_IS",
+                           "5_LC_ShelfBreak"), sep=""))
+names2<-c(paste("abund_", c("1_IS","2_OS",
                             "3_GB", "4_Slope", 
-                            "5_SW_margin_GB"), sep=""))
-names3<-c(paste("indval_", c("1_OS","2_IS",
+                            "5_LC_ShelfBreak"), sep=""))
+names3<-c(paste("indval_", c("1_IS","2_OS",
                              "3_GB", "4_Slope", 
-                             "5_SW_margin_GB"), sep=""))
+                             "5_LC_ShelfBreak"), sep=""))
 names(relfreq)<-names1
 names(relabund)<-names2
 names(indval)<-names3
