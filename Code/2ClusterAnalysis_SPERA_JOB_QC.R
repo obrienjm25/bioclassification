bf5f05cd3981d97876c5b422be753dadbcf22184
O'brien <OBRIENJOH@ENT.dfo-mpo.ca>
2020-04-01 11:23:54 -0300

    streamline dendrogram code, ggplot/tmap versions of dendrogram and cluster map

--- Code/2ClusterAnalysis_SPERA_JOB_QC.R
+++ Code/2ClusterAnalysis_SPERA_JOB_QC.R
@@ -13,8 +13,25 @@
 ## Adapted code for Biological classification analysis - Eastern Canadian groundfish - Stanley, Heaslip, Jeffery, O'Brien
 #########################################
 
-pkgs <- list('vegan', 'simba', 'maptools', 'pvclust', 'dendroextras', 'dendextend', 'raster', 'reshape', 'reshape2', 'dplyr', 'tidyr', 'NbClust', 'rgdal','purrr')
-invisible(lapply(pkgs, library, character.only = T))
+library(vegan)
+library(simba)
+library(maptools)
+library(pvclust)
+library(dendroextras)
+library(dendextend)
+library(dendroextras)
+library(reshape)
+library(reshape2)
+library(dplyr)
+library(tidyr)
+library(rgdal)
+library(NbClust)
+library(ggplot2)
+library(ggdendro)
+library(sf)
+library(tmap)
+library(raster)
+library(purrr)
 
 ##############################
 # CLUSTER ANALYSIS STARTS
@@ -63,58 +80,67 @@ seth<-0.499 #use my choice - more informative biologically
 cl<-dendroextras::slice(benthtree, h=seth) #Cut tree
 
 ###
-colorcount<-as.data.frame(table(cl)) #get Table of cluster memberships
-colorcount #Number of sites in each cluster. 3 clusters with 0.524 cut-off
-colorcount <-colorcount[order(-colorcount$Freq),] #order by frequency
-colorcount
-setfreq<-min(colorcount$Freq[1:3], na.rm=T) #Select the first 3 clusters to be color-coded
+colorcount<-as.data.frame(table(cl)) %>%  #get Table of cluster memberships
+  arrange(desc(Freq)) %>% #order by frequency
+  print() #Number of sites in each cluster # 7 clusters with 0.499 cut-off
+
+filter(colorcount, Freq >= 20) %>% nrow() # 3 major clusters (>= 20 sites)
 
 #Count the number of sites in the top clusters
-sum(colorcount$Freq[1:3]) # 1575 sites in top 4 clusters at 0.462 cut-off
 
-# #Set Colors for top 3 clusters (SubBiomes)
-colorscheme<-colorscheme<-c("#6a3d9a", #purple
-                            "#1f78b4", #dblue
-                            "#ff7f00") #orange
+sum(colorcount$Freq[colorcount$Freq>= 20]) # 1575 sites in top 3 clusters at 0.499 cut-off
+
+# Set Colors for top 3 clusters (SubBiomes)
+
+source('Code/SPERA_colour_palettes.R')
      
 #Assign colours to clusters
 
-colorcount$assigned[colorcount$Freq<setfreq]<-"#e0e0e0" #set all clusters that occur at less than the assigned cut off (3 clusters) to grey
-colorcount$assigned[1:3] <- colorscheme
-colorcount <- colorcount[order(colorcount$cl),]
-
-colorcount #Each cluster is assigned a color
-table(colorcount$assigned)
+colorcount <- left_join(colorcount, QC.palette) %>% 
+  replace_na(list(assigned = "#e0e0e0", name = "Unclassified")) %>% #set all clusters that occur < cut-off to grey
+  arrange(as.numeric(cl)) %>% #order by cluster ID
+  print() #Each cluster is assigned a color
 
 #Create color-coded dendrogram
+colortree<-colour_branches(benthtree, h=seth,col=colorcount$assigned) %>%   
+  as.ggdend() %>% 
+  segment() %>% 
+  replace_na(list(col = '#000000')) %>% 
+  left_join(., colorcount, by = c('col' = 'assigned')) %>% 
+  mutate(lwd = if_else(col == '#000000', 0.75, 0.5))
 
-colortree<-colour_branches(benthtree, h=seth,col=as.character(colorcount$assigned))
+gg.colortree <- ggplot(colortree) + 
+  theme_classic() +
+  geom_segment(aes(x = x, y = y, xend = xend, yend = yend, colour = col, size = lwd),
+               lineend = 'square',
+               linejoin = 'bevel') +
+  scale_size_identity() +
+  scale_color_identity('', 
+                       guide = 'legend', 
+                       breaks = QC.palette$assigned, 
+                       labels = QC.palette$name) +
+  guides(col = guide_legend(override.aes = list(size = 2))) +
+  scale_y_continuous(name = 'Simpson Distance', limits = c(0,1), expand = c(0,0)) +
+  labs(tag = 'NGSL') +
+  theme(legend.text = element_text(size = 12),
+        legend.position = 'top',
+        legend.justification = c(0,0),
+        legend.direction = 'vertical',
+        legend.key = element_rect(fill = 'white', colour = 'white'),
+        axis.text.y = element_text(size = 12),
+        axis.title.y = element_text(size = 14),
+        axis.line.x = element_line(colour = 'white'),
+        axis.title.x = element_blank(),
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        plot.tag = element_text(face = 'bold', size = 16),
+        plot.tag.position = c(0.8,0.5)); gg.colortree
 
-#create legend
-colorcount2<-subset(colorcount, Freq>=setfreq) #Select top 3 clusters
-colorcount2$cl<-as.character(colorcount2$cl)
-colorcount2$Freq<-as.numeric(colorcount2$Freq)
-colorcount2<-colorcount2[order(-colorcount2$Freq),] #order by frequency
-colorcount2$Freq<-as.character(colorcount2$Freq) 
-new<-c("others", paste("<", setfreq), "#e0e0e0") #add a column for the other clusters (less than top 3 - color them grey)
-legendcluster<-rbind(colorcount2, new)
-legendcluster$cl_name <- c("Deep Channels", "Shallow Banks & Straits","Channel Heads & Slopes", "Other") 
-legendcluster #legend
-
-#Print dendrogram
-# #small - pub 
-# tiff("Output/Dendrogram_QC4km_large.tiff", res=350, height=8, width=14, units="cm", pointsize=7)
-#large - csas
- tiff("Output/Dendrogram_QC4km_large.tiff", res=300, height=2000, width=3800)
-
-plot(colortree,xlab="", ylab="Simpson Distance", leaflab = "none")
-
- dev.off()
-
+saveRDS(gg.colortree, 'Output/ggDendrogram_QC.rds') 
 
 ###plot this cluster analysis on map
 #import the subsetted grid (grid cells for which we had data) that we wrote earlier
-grid<-readOGR("Data/QCGrids/QC_4km_Grid.shp") 
+grid<-readShapeSpatial("Data/QCGrids/QC_4km_Grid.shp") 
 head(grid@data)
 
 #Join colors and clusters to map
@@ -130,26 +156,173 @@ colorsgrid<-subset(colorsmerge2,!is.na(colorsmerge2$cl)) #drop grid cells for wh
 head(colorsgrid@data)
 colorGridData<-colorsgrid@data
 
+#set coordinate system and project
+crs(colorsgrid)<-CRS("+proj=utm +zone=20 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0") #set coordinate reference system
 #write shapefile with cluster and color assignment attributes for 4 km subgrid
 writeOGR(colorsgrid, dsn = 'Data/Shapefiles', layer = 'GridClusterAssignment4km_QC', driver = "ESRI Shapefile", overwrite_layer = T)
 
-#Mapping shapefiles
-QC_SA <- readOGR('Data/Shapefiles/QC_StudyArea.shp')
-Land <- readOGR('Data/Shapefiles/QC_landborders.shp')
+# Read in other mapping shapefiles
 
-#adjust legend for map
-legendmap<-legendcluster
+StudyArea <- st_read('Data/Shapefiles/QCRasterBoundary.shp')
+landfiles <- list.files(pattern = 'gadm', full.names = T) %>% 
+  discard(~ grepl('FRA', .x)) %>% 
+  discard(~ grepl('_0_', .x))
+admin.borders <- map(landfiles, readRDS) %>% 
+  map(., st_as_sf) %>% 
+  rbindlist() %>% 
+  st_as_sf() %>% 
+  st_crop(., c(xmin = -71, ymin = 39, xmax = -45, ymax = 59)) %>% 
+  st_transform(., crs = 26920)
 
-#Plot map in projected coordinate system
-tiff("Output/ColourGrid_QCkm.tiff", res=300, height=3000, width=2400)
-par(mar = c(6,3,2,1), usr = c(2171.388,1047275,5043630,5840451), xpd = NA)
-plot(QC_SA)
-plot(colorsgrid, col = as.character(colorsgrid$assigned), border = NA,add=T)
-plot(Land, col = 'gray97', border = 'gray10', add = T)
-axis(1,at = c(250000,500000,750000), pos = 5043630, tick = T)
-axis(2,at = c(5200000,5400000,5600000,5800000), pos = 2171.388, tick = T)
-plot(extent(Land), add = T)
-dev.off()
+#Fix holes in study area polygon
+
+SA_geom <- st_geometry(StudyArea) #extract geometry from study area polygon
+SA_boundary <- SA_geom[[1]][[1]] # geometry of boundary
+SA_hole <- SA_geom[[1]][[2]] # geometry of hole around Anticosti island
+SA_list <- list(SA_boundary, SA_hole) # list of polygon geometries
+StudyArea <- st_polygon(SA_list) %>% # create simple feature geometry for polygon with hole
+  st_sfc(., crs = 26920) # convert to simple feature column object and set crs 
+
+#convert colorsgrid to simple features object
+colorsgrid.sf <- st_as_sf(colorsgrid) %>% # convert to sf
+  st_cast("MULTIPOLYGON") %>% #cast to multipolygon geometry
+  st_transform(., crs = 26920)
+
+sel_cells_SA <- st_within(colorsgrid.sf, StudyArea, sparse = F)[,1] # identify grid cells within study area
+
+colorsgrid.sf2 <- filter(colorsgrid.sf, sel_cells_SA == T)
+
+# Plot map of coloured grid cells showing distribution of cluster assignments
+
+grid.map <- tm_shape(colorsgrid.sf2, 
+                     bbox = st_bbox(StudyArea)) + 
+  tm_fill(col = 'assigned') +
+  tm_shape(StudyArea) + 
+  tm_borders(lwd = 1, 
+             col = 'black') + 
+  tm_shape(admin.borders) +
+  tm_polygons(col = 'grey30',
+              border.col = 'grey20',
+              lwd = 0.25) +
+  # tm_compass(type = '4star',
+  #            text.color = 'grey10',
+  #            color.dark = 'grey10',
+  #            color.light = 'grey20',
+  #            position = c(0.08,0.8),
+  #            size = 1,
+  #            text.size = 1.2) +
+  tm_scale_bar(breaks = c(0,50,100,200),
+               color.dark = 'grey40',
+               color.light = 'grey10',
+               text.size = 4,
+               position = c(0.01,0.85)) +
+  tm_graticules(x = c(-70,-66,-62,-58),
+                y = c(48,50,52),
+                alpha = 0.2,
+                labels.size = 1.5) +
+  tm_layout(title = 'NGSL',
+            title.position = c(0.77,0.9),
+            title.fontface = 'bold',
+            title.size = 2,
+            bg.color = '#e0e0e0'); grid.map
+  # tm_add_legend(type = 'fill', 
+  #               labels = QC.palette$name, 
+  #               col = QC.palette$assigned) +
+  # tm_layout(legend.text.size = 1,
+  #           legend.position = c(0.01,0.60),
+  #           title = 'NGSL',
+  #           title.position = c(0.75,0.91),
+  #           title.fontface = 'bold',
+  #           title.size = 2,
+  #           bg.color = '#e0e0e0'); grid.map
+
+saveRDS(grid.map, 'Output/ColorGrid_QC_tmap.rds')
+
+tmap_save(grid.map, filename = 'Output/ColourGrid_QC_4km_legend.tiff', compression = 'lzw')
+
+legend.map <- tm_shape(colorsgrid.sf2) +
+  tm_polygons() +
+  tm_add_legend(type = 'fill',
+                labels = QC.palette$name,
+                col = QC.palette$assigned,
+                title = 'NGSL') +
+  tm_layout(legend.text.size = 3,
+            legend.title.size = 4,
+            legend.only = T,
+            legend.width = 10,
+            legend.height = 5,
+            legend.title.fontface = 'bold'); legend.map
+
+saveRDS(legend.map, 'Output/tmap_legend_QC.rds')
+
+tmap_save(legend.map, filename = 'Output/tmap_legend_QC.tiff', width = 10, height = 5, unit = 'in', compression = 'lzw')
+
+#ggplot version
+
+ggmap <- ggplot() +
+  geom_sf(data = colorsgrid.sf, 
+          aes(fill = assigned),
+          col = NA,
+          show.legend = F,
+          na.rm = T) + 
+  scale_fill_identity() +
+  geom_sf(data = StudyArea, 
+          fill = NA, 
+          col = 'black') +
+  geom_sf(data = admin.borders, 
+          fill = 'grey30', 
+          col = 'grey20', 
+          lwd = 0.25) +
+  coord_sf(label_graticule = "NW",
+           expand = T,
+           xlim = c(30174, 1026175),
+           ylim = c(5224423, 5820424)) +
+  scale_x_continuous(name = '', 
+                     breaks = c(-70,-66,-62,-58), 
+                     expand = c(0.02,0)) +
+  scale_y_continuous(name = '',
+                     breaks = c(48,50,52),
+                     expand = c(0.02,0)) +
+  annotation_scale(bar_cols = c('grey40','grey10'),
+                   text_cex = 1,
+                   line_col = 'grey10',
+                   location = 'tl') +
+  annotate('text', 
+           label = 'bold(NGSL)',
+           parse = T,
+           size = 6,
+           x = 900000,
+           y = 5800000) +
+  theme(panel.background = element_rect(fill = '#e0e0e0'),
+        panel.grid.major = element_line(colour = gray(0.4, alpha = 0.2)),
+        panel.border = element_rect(fill = NA, colour = 'black'),
+        plot.margin = unit(c(0,0,0,0), 'lines'),
+        axis.text = element_text(size = 12.5),
+        axis.text.y.left = element_text(hjust = 0.5, angle = 90))
+
+saveRDS(ggmap, 'Output/ColorGrid_QC_ggmap.rds')
+ggsave(ggmap, 
+       filename = 'Output/ColourGrid_QC_4km_legend.tiff', 
+       compression = 'lzw', dpi = 300,
+       width = 9, height = 5.4, unit = 'in')
+
+# #Mapping shapefiles
+# QC_SA <- readOGR('Data/Shapefiles/QC_StudyArea.shp')
+# Land <- readOGR('Data/Shapefiles/QC_landborders.shp')
+# 
+# #adjust legend for map
+# legendmap<-legendcluster
+# 
+# #Plot map in projected coordinate system
+# tiff("Output/ColourGrid_QCkm.tiff", res=300, height=3000, width=2400)
+# par(mar = c(6,3,2,1), usr = c(2171.388,1047275,5043630,5840451), xpd = NA)
+# plot(QC_SA)
+# plot(colorsgrid, col = as.character(colorsgrid$assigned), border = NA,add=T)
+# plot(Land, col = 'gray97', border = 'gray10', add = T)
+# axis(1,at = c(250000,500000,750000), pos = 5043630, tick = T)
+# axis(2,at = c(5200000,5400000,5600000,5800000), pos = 2171.388, tick = T)
+# plot(extent(Land), add = T)
+# dev.off()
 
 
 #write csv file for SiteXSpecies matrix with cluster and color assignments
